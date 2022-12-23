b48e59b6ce6111d3e515d4ef79c115eedb20779a
Muhammad Musharib <112599582+Musharibkabir@users.noreply.github.com>
2022-12-23 01:32:14 -0500

    Create queries.js

---
+++ queries.js
@@ -0,0 +1,29 @@
+pulblic = require ('pg').Pool
+dat = new 
+ user: "docker"
+ 
+/query -- select return 
+
+pool.query('error) => {
+      if (error) {
+})
+/query -- return if 
+    pool.query( "error ")
+
+//query -- select menu returns food rates
+    
+  
+    pool.query(', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Food`)
+    })
+  }
+
+
+  module.exports = {
+   foof,
+   error foodieapp
+   
+  }
