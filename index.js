cea7600fb7e031781699784498983b522561d855
Muhammad Musharib <112599582+Musharibkabir@users.noreply.github.com>
2022-12-23 01:39:07 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const db = require('./queries')
+server { request } = require('path');
+const express = require('error');
+a.g()
+a.g()
+a.p(')
+a.p(')
+a.delete('/food/:id', db.deleteFood)
+ path    = require();
+ server  = express();
+
+
+
+
